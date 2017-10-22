270dc4a4076342fb7675a5862ac026aeb0a3be13
Matthew Phillips <modsobgaming@gmail.com>
2017-10-21 21:57:58 -0400

    Added camera scripts for 3/4 top-down and 3rd-person view. TODO: Zoom clamping. BUG: Switching to 3rd-person first time places the camera higher than intended. -MP

---
+++ Simple Tactics/Assets/Scripts/godCamScript.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class godCamScript : MonoBehaviour
+{
+    // Bool for which camera is active
+    [SerializeField]
+    bool godCamActive = true;
+
+    // Reference to the camera object
+    public Camera cam;
+
+    // Temporary target objects. These will be passed in from the list of characters in the map.
+    public GameObject tempTarget;
+    public GameObject tempTarget2eb;
+
+    // Vectors
+    Vector3 newCamPos;
+
+    // Default overhead cam position offset
+    Vector3 camOffset = new Vector3(0, 10, -10);
+    Vector3 camRotation = new Vector3(45, 0, 0);
+
+    float camSpeed = 3.0f;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Set default rotation (45 degrees on the X axis)
+        cam.transform.Rotate(camRotation);
+
+        // Connect with the list of characters in the map
+        // If no characters are found, set target to Origin
+        // else set target to active character
+
+        // temp default starting spot
+        TeleportFocus(cam.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Mirrors the code in shoulderCamScript, switches camera mode
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            godCamActive = !godCamActive;
+        }
+
+        if (godCamActive)
+        {
+            // ensure the camera is at the correct rotation
+            cam.transform.rotation = Quaternion.Euler(camRotation);
+
+            // Called every frame to move the camera to the active target
+            LerpFocus();
+            DetectScroll();
+
+            // Debug code
+            if (Input.GetKey(KeyCode.A))
+                SetNewCamPosition(tempTarget.transform);
+            if (Input.GetKey(KeyCode.D))
+                SetNewCamPosition(tempTarget2eb.transform);
+        }
+    }
+
+    void DetectScroll()
+    {
+
+        if (Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+        {
+            camOffset -= new Vector3(0, 1, -1) * Time.deltaTime * 100.0f;
+            newCamPos -= new Vector3(0, 1, -1) * Time.deltaTime * 100.0f;
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") < 0)
+        {
+            camOffset += new Vector3(0, 1, -1) * Time.deltaTime * 100.0f;
+            newCamPos += new Vector3(0, 1, -1) * Time.deltaTime * 100.0f;
+        }
+    }
+
+    void TeleportFocus(Transform _trans)
+    {
+        cam.transform.position = _trans.position + camOffset;
+    }
+
+    void SetNewCamPosition(Transform _trans)
+    {
+        newCamPos = _trans.position + camOffset;
+    }
+
+    void LerpFocus()
+    {
+        if (cam.transform.position != newCamPos)
+            cam.transform.position += (newCamPos - cam.transform.position) * (Time.deltaTime * camSpeed);
+    }
+}
---
+++ Simple Tactics/Assets/Scripts/shoulderCamScript.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class shoulderCamScript : MonoBehaviour
+{
+    // Bool for which camera is active
+    [SerializeField]
+    bool shoulderCamActive = false;
+    bool switchMode = false;
+
+    // Target character to attach the camera to
+    public GameObject tempTarget, tempTarget2eb;
+    GameObject currTarget;
+
+    // The camera object
+    public Camera cam;
+
+    // Camera vectors
+    [SerializeField]
+    Vector3 newCamPosition;
+    Vector3 targetPos;
+
+    // Quaternion for rotation storage
+    Quaternion lookAt;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        // Target must be set before running
+        currTarget = tempTarget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            shoulderCamActive = !shoulderCamActive;
+            if (shoulderCamActive)
+            {
+                AttachToTarget(currTarget.transform);
+                switchMode = true;
+            }
+        }
+
+        if (shoulderCamActive)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                AttachToTarget(tempTarget.transform);
+                currTarget = tempTarget;
+                switchMode = true;
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                AttachToTarget(tempTarget2eb.transform);
+                currTarget = tempTarget2eb;
+                switchMode = true;
+            }
+
+            if (switchMode)
+            {
+                LerpFocus();
+            }
+            else
+            {
+                MouseYRotation();
+                ZoomTarget();
+            }
+        }
+    }
+
+    void AttachToTarget(Transform _trans)
+    {
+        newCamPosition = _trans.position - (cam.transform.forward * 3.0f) + new Vector3(0, 1, 0);
+        //cam.transform.position = newCamPosition;
+        lookAt = Quaternion.LookRotation(_trans.forward);
+        //cam.transform.rotation = lookAt;
+        targetPos = _trans.position;
+    }
+
+    void MouseYRotation()
+    {
+        if (Input.GetButton("Fire2"))
+        {
+
+            if (Input.GetAxisRaw("Mouse X") > 0)
+            {
+                cam.transform.RotateAround(targetPos, cam.transform.up, Time.deltaTime * 100.0f);
+                //cam.transform.Rotate(cam.transform.up * (-Time.deltaTime * 100.0f));
+                lookAt = cam.transform.rotation;
+            }
+
+            else if (Input.GetAxisRaw("Mouse X") < 0)
+            {
+                cam.transform.RotateAround(targetPos, cam.transform.up, Time.deltaTime * -100.0f);
+                //cam.transform.Rotate(cam.transform.up * (Time.deltaTime * 100.0f));
+                lookAt = cam.transform.rotation;
+            }
+        }
+        currTarget.transform.rotation = lookAt;
+        newCamPosition = cam.transform.position;
+    }
+
+    void LerpFocus()
+    {
+        if (cam.transform.position != newCamPosition)
+            cam.transform.position += (newCamPosition - cam.transform.position) * (Time.deltaTime * 10.0f);
+        if (cam.transform.rotation != lookAt)
+            cam.transform.rotation = Quaternion.Slerp(cam.transform.rotation, lookAt, Time.time * 0.1f);
+
+        if (cam.transform.position == newCamPosition && cam.transform.rotation == lookAt)
+            switchMode = false;
+    }
+
+    void ZoomTarget()
+    {
+        if(Input.GetAxisRaw("Mouse ScrollWheel") < 0)
+        {
+            newCamPosition -= currTarget.transform.forward * (Time.deltaTime * 100.0f);
+            cam.transform.position = newCamPosition;
+        }
+        else if (Input.GetAxisRaw("Mouse ScrollWheel") > 0)
+        {
+            newCamPosition += currTarget.transform.forward * (Time.deltaTime * 100.0f);
+            cam.transform.position = newCamPosition;
+        }
+    }
+}
