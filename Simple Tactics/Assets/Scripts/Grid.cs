0ca4ffd741acfa8160b830ce63cb4ad43f88ce9b
Noel Ortiz <NOrtiz678@gmail.com>
2017-11-15 21:50:41 -0500

    grid and tiles. Tiles have energy and type as well as base values set up. Getters and Setters are set up as well

---
+++ Simple Tactics/Assets/Scripts/Grid.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    List<Vector3> mapGrid;
+    Vector3 pos;
+	// Use this for initialization
+	void Start ()
+    {
+        mapGrid = new List<Vector3>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void createGridPoints()
+    {
+        
+    }
+
+    void instantiateTheGrid()
+    {
+
+    }
+
+
+}
---
+++ Simple Tactics/Assets/Scripts/Tile.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+
+    character thePlayer;
+    bool passable;
+    public enum tileType
+    {
+        inert, leyline, changeable
+    }
+    public enum tileEnergy
+    {
+         heat, cold, death, life, none
+    }
+
+    public tileEnergy thisTilesEnergy;
+    
+    tileEnergy getTileEnergy()
+    {
+        return thisTilesEnergy;
+    }
+    bool getTilePassable()
+    {
+        return passable;
+    }
+    void setTilePassable(bool _value)
+    {
+        passable = _value;
+    }
+    //0 = heat, 1 = cold, 2 = death, 3 = life, 4 = none
+     void setTilesEnergy( int energyValue)
+    {
+        if(energyValue >= 0 && energyValue <= 4)
+        {
+            thisTilesEnergy = (tileEnergy)energyValue;
+        }
+        else
+        {
+            thisTilesEnergy = (tileEnergy)4;
+        }
+       
+    }
+	// Use this for initialization
+	void Start ()
+    {
+        setTilesEnergy(4);
+        setTilePassable(true);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        
+	}
+
+}
