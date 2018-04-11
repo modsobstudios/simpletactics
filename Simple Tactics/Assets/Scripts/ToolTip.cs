e0786844b00d55c571b2d0dcaa2c62aeea5d3f48
JLathey <jalathey@fullsail.edu>
2018-04-10 22:56:55 -0400

    edited button stuff

---
+++ Simple Tactics/Assets/Scripts/ToolTip.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToolTip : MonoBehaviour {
+    private Transform target;
+    private TextMesh textMesh;
+
+    // Use this for initialization
+    void Start () {
+        target = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.rotation = target.rotation;
+    }
+}
