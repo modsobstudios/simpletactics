dad695476b33a940618d5e45bd0f999e86d520f9
Noel Ortiz <NOrtiz678@gmail.com>
2018-04-24 20:23:22 -0400

    added in a menu controller

---
+++ Simple Tactics/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    void LoadCampaginSelection()
+    {
+
+    }
+
+    void LoadEndlessMode()
+    {
+
+    }
+
+    void LoadOptionsMenu()
+    {
+
+    }
+
+    void LoadSettingsMenu()
+    {
+
+    }
+
+    void ExitGame()
+    {
+
+    }
+}
